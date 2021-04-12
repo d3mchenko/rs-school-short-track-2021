f25724146fd6822ca88974d044cea7d47366d072
d3mchenko <maksim-ignatyuk@mail.ru>
2021-04-13 02:14:35 +0300

    fix: update short-track

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,16 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let index = 0;
+  let j = array.length;
+  let n;
+  while (index < j) {
+    n = Math.floor((j + index) / 2);
+    if (array[n] >= value) j = n;
+    else index = n + 1;
+  }
+  return index;
 }
 
 module.exports = findIndex;
